9ccf80d10b47024dd9b9c682b4a3656a3973cdd7
kotegg <lolpol.kek123@gmail.com>
2023-08-14 16:39:15 +0300

    2

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        if (month >=13 && month <= 0)
+        if (month >=13 || month <= 0)
         {
             System.out.println("wrong number!");
             return;
