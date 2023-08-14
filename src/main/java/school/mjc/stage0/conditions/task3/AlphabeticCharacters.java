b813018181f31beeee0b0aa9055b180bfa9bdabd
kotegg <lolpol.kek123@gmail.com>
2023-08-14 16:25:00 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!((character >= 65 && character <= 90) || (character >= 97 && character <= 122)))
+        {
+            System.out.println("wrong alphabet!");
+            return;
+        }
+        if (character == 'a' || character == 'A' || character == 'e' || character == 'E' || character == 'i' ||
+                character == 'I' || character == 'o' || character == 'O' || character == 'u' || character == 'U' ||
+                character == 'y' || character == 'Y') System.out.println("Vowel");
+        else System.out.println("Consonant");
     }
 }
