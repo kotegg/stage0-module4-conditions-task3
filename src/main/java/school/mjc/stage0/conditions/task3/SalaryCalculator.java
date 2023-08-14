e81ea12b3d6b45ea7ecf959e7e7d5964439f866f
kotegg <lolpol.kek123@gmail.com>
2023-08-14 16:40:48 +0300

    3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if (salary < 0) return;
+        if (salary < 0)
+        {
+            System.out.println("wrong input!");
+            return;
+        }
         if (salary <= 10000) System.out.println(salary - salary * 0.15);
         if (salary > 10000 && salary <= 20000) System.out.println(salary - salary * 0.18);
         if (salary > 20000) System.out.println(salary - salary * 0.20);
